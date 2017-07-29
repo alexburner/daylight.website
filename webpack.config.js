89f00bf3f6564e3ce3b112b47416739879d19e05
Alex Burner <alex.burner@gmail.com>
2017-07-29 11:45:33 -0700

    eval sourcemaps for compiled code in inspector

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
 	},
 
 	// Enable sourcemaps for debugging webpack's output.
-	devtool: 'cheap-module-source-map',
+	devtool: 'eval-cheap-module-source-map',
 
 	resolve: {
 		// Add '.ts' and '.tsx' as resolvable extensions.
