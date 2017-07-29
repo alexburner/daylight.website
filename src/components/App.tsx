da7cfe0dd56379d56fb7040880b776e4a12d7fbf
Alex Burner <alex.burner@gmail.com>
2017-07-29 11:46:10 -0700

    basic get geolocation & calc sun times

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,11 +3,58 @@ import * as suncalc from 'suncalc';
 
 console.log(suncalc);
 
-export default class App extends React.Component<undefined, undefined> {
+interface Times {
+    sunrise: Date;
+    sunriseEnd: Date;
+    goldenHourEnd: Date;
+    solarNoon: Date;
+    goldenHour: Date;
+    sunsetStart: Date;
+    sunset: Date;
+    dusk: Date;
+    nauticalDusk: Date;
+    night: Date;
+    nadir: Date;
+    nightEnd: Date;
+    nauticalDawn: Date;
+    dawn: Date;
+}
+
+interface State {
+    latitude: number|null;
+    longitude: number|null;
+    times: Times|null;
+}
+
+export default class App extends React.Component<{}, State> {
+    constructor() {
+        super();
+        this.state = {
+            latitude: null,
+            longitude: null,
+            times: null,
+        };
+
+        // Get client geolocation
+        navigator.geolocation.getCurrentPosition((pos: Position) => {
+            const latitude = pos.coords.latitude;
+            const longitude = pos.coords.longitude;
+            const times = suncalc.getTimes(new Date(), latitude, longitude);
+            this.setState({ latitude, longitude, times });
+        }, (e: PositionError) => alert(e.message));
+    }
+
     render() {
+        if (!this.state.times) return <div>Waiting for geolocation...</div>;
         return (
             <div>
-                Hello world
+                <p>
+                    latitude = {this.state.latitude},
+                    longitude = {this.state.longitude}
+                </p>
+                <code>
+                    {JSON.stringify(this.state.times)}
+                </code>
             </div>
         );
     }
