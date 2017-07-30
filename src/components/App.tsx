96df73ed53fc552b049d9f052007778235bc3a29
Alex Burner <alex.burner@gmail.com>
2017-07-29 18:20:04 -0700

    tweaks

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -59,9 +59,11 @@ interface Coord {
 }
 
 interface State {
-    latitude: number|null;
-    longitude: number|null;
-    times: Times|null;
+    latitude: number | null;
+    longitude: number | null;
+    times: Times | null;
+    angles: Angles | null;
+    coords: Coords | null;
 }
 
 const WIDTH = 400;
@@ -70,6 +72,16 @@ const RADIUS = 180;
 const CX = WIDTH / 2;
 const CY = HEIGHT / 2;
 
+const colors = {
+    day: '#fff6ab',
+    golden: '#ffe881',
+    horizon: '#f57b75',
+    civil: '#1c458a',
+    nautical: '#062a67',
+    astronomical: '#071c40',
+    night: '#111',
+};
+
 export default class App extends React.Component<{}, State> {
     constructor() {
         super();
@@ -77,13 +89,40 @@ export default class App extends React.Component<{}, State> {
             latitude: null,
             longitude: null,
             times: null,
+            angles: null,
+            coords: null,
         };
     }
 
+    componentDidMount() {
+        // Get client geolocation
+        if (this.state.times) return; // unless we already have it
+        navigator.geolocation.getCurrentPosition((pos: Position) => {
+            const latitude = pos.coords.latitude;
+            const longitude = pos.coords.longitude;
+            const times = suncalc.getTimes(new Date(), latitude, longitude);
+            const angles = getAngles(times);
+            const coords = getCoords(angles, RADIUS, CX, CY);
+            this.setState({ latitude, longitude, times, angles, coords });
+        }, (e: PositionError) => alert(e.message));
+    }
+
     render() {
-        if (!this.state.times) return <div>Waiting for geolocation...</div>;
-        const angles = getAngles(this.state.times);
-        const coords = getCoords(angles, RADIUS, CX, CY);
+        const coords = this.state.coords;
+        if (!coords) {
+            return (
+                <div
+                    style={{
+                        textAlign: 'center',
+                        transform: 'translateY(-50%)',
+                        position: 'relative',
+                        top: '40%',
+                    }}
+                >
+                    Waiting for geolocation...
+                </div>
+            );
+        }
         return (
             <div style={{ padding: '60px 20px'}}>
                 <svg
@@ -95,91 +134,87 @@ export default class App extends React.Component<{}, State> {
                         margin: 'auto'
                     }}
                 >
-                    {[
-                        getTopCap({
-                            // daylight
-                            left: coords.goldenHourEnd,
-                            right: coords.goldenHour,
-                            radius: RADIUS,
-                            color: '#fff6ab',
-                        }),
-                        getSegment({
-                            // golden hour
-                            ul: coords.goldenHourEnd,
-                            ur: coords.goldenHour,
-                            ll: coords.sunriseEnd,
-                            lr: coords.sunsetStart,
-                            radius: RADIUS,
-                            color: '#ffe881',
-                        }),
-                        getSegment({
-                            // sunrise/sunset
-                            ul: coords.sunriseEnd,
-                            ur: coords.sunsetStart,
-                            ll: coords.sunrise,
-                            lr: coords.sunset,
-                            radius: RADIUS,
-                            color: '#f57b75',
-                        }),
-                        getSegment({
-                            // civil twilight
-                            ul: coords.sunrise,
-                            ur: coords.sunset,
-                            ll: coords.dawn,
-                            lr: coords.dusk,
-                            radius: RADIUS,
-                            color: '#1c458a',
-                        }),
-                        getSegment({
-                            // nauticaul twilight
-                            ul: coords.dawn,
-                            ur: coords.dusk,
-                            ll: coords.nauticalDawn,
-                            lr: coords.nauticalDusk,
-                            radius: RADIUS,
-                            color: '#062a67',
-                        }),
-                        getSegment({
-                            // astronomical twilight
-                            ul: coords.nauticalDawn,
-                            ur: coords.nauticalDusk,
-                            ll: coords.nightEnd,
-                            lr: coords.night,
-                            radius: RADIUS,
-                            color: '#071c40',
-                        }),
-                        getBottomCap({
-                            // night
-                            left: coords.night,
-                            right: coords.nightEnd,
-                            radius: RADIUS,
-                            color: '#111',
-                        }),
-                    ]}
+                    <defs>
+                        <clipPath id="clip-disc">
+                            <circle cx={CX} cy={CY} r={RADIUS} />
+                        </clipPath>
+                    </defs>
+                    <g clipPath="url(#clip-disc)">
+                        {[
+                            getTopCap({
+                                // daylight
+                                left: coords.goldenHourEnd,
+                                right: coords.goldenHour,
+                                color: colors.day,
+                                radius: RADIUS,
+                            }),
+                            getSegment({
+                                // golden hour
+                                ul: coords.goldenHourEnd,
+                                ur: coords.goldenHour,
+                                ll: coords.sunriseEnd,
+                                lr: coords.sunsetStart,
+                                color: colors.golden,
+                                radius: RADIUS,
+                            }),
+                            getSegment({
+                                // sunrise/sunset
+                                ul: coords.sunriseEnd,
+                                ur: coords.sunsetStart,
+                                ll: coords.sunrise,
+                                lr: coords.sunset,
+                                color: colors.horizon,
+                                radius: RADIUS,
+                            }),
+                            getSegment({
+                                // civil twilight
+                                ul: coords.sunrise,
+                                ur: coords.sunset,
+                                ll: coords.dawn,
+                                lr: coords.dusk,
+                                color: colors.civil,
+                                radius: RADIUS,
+                            }),
+                            getSegment({
+                                // nauticaul twilight
+                                ul: coords.dawn,
+                                ur: coords.dusk,
+                                ll: coords.nauticalDawn,
+                                lr: coords.nauticalDusk,
+                                color: colors.nautical,
+                                radius: RADIUS,
+                            }),
+                            getSegment({
+                                // astronomical twilight
+                                ul: coords.nauticalDawn,
+                                ur: coords.nauticalDusk,
+                                ll: coords.nightEnd,
+                                lr: coords.night,
+                                color: colors.astronomical,
+                                radius: RADIUS,
+                            }),
+                            getBottomCap({
+                                // night
+                                left: coords.night,
+                                right: coords.nightEnd,
+                                color: colors.night,
+                                radius: RADIUS,
+                            }),
+                        ]}
+                    </g>
                     <circle
                         cx={CX}
                         cy={CY}
                         r={RADIUS}
                         style={{
                             fill: 'transparent',
-                            stroke: '#222',
-                            strokeWidth: 6,
+                            stroke: '#444',
+                            strokeWidth: 5,
                         }}
                     />
                 </svg>
             </div>
         );
-    }
-
-    componentDidMount() {
-        // Get client geolocation
-        if (this.state.times) return; // unless we already have it
-        navigator.geolocation.getCurrentPosition((pos: Position) => {
-            const latitude = pos.coords.latitude;
-            const longitude = pos.coords.longitude;
-            const times = suncalc.getTimes(new Date(), latitude, longitude);
-            this.setState({ latitude, longitude, times });
-        }, (e: PositionError) => alert(e.message));
     }
 }
 
@@ -219,8 +254,8 @@ const getSegment = ({ ul, ur, ll, lr, radius, color }: {
     ur: Coord;
     ll: Coord;
     lr: Coord;
-    radius: number,
     color: string;
+    radius: number,
 }): JSX.Element => getPath(color, `
     M ${ul.x} ${ul.y}
     A ${radius} ${radius} 0 0 1 ${ll.x} ${ll.y}
@@ -232,8 +267,8 @@ const getSegment = ({ ul, ur, ll, lr, radius, color }: {
 const getTopCap = ({ left, right, radius, color }: {
     left: Coord,
     right: Coord,
-    radius: number,
     color: string,
+    radius: number,
 }): JSX.Element => getPath(color, `
     M ${left.x} ${left.y}
     A ${radius} ${radius} 0 1 0 ${right.x} ${right.y}
@@ -243,8 +278,8 @@ const getTopCap = ({ left, right, radius, color }: {
 const getBottomCap = ({ left, right, radius, color }: {
     left: Coord,
     right: Coord,
-    radius: number,
     color: string,
+    radius: number,
 }): JSX.Element => getPath(color, `
     M ${left.x} ${left.y}
     A ${radius} ${radius} 0 0 0 ${right.x} ${right.y}
@@ -252,5 +287,5 @@ const getBottomCap = ({ left, right, radius, color }: {
 `);
 
 const getPath = (color: string, d: string): JSX.Element => (
-    <path style={{ fill: color, stroke: color, strokeWidth: 2 }} d={d} />
+    <path d={d} style={{ fill: color, stroke: color, strokeWidth: 2 }} />
 );
