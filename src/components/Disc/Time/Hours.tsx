dd5a65a135fd4bccc7d3fdd9b6c28b6d5de67106
Alex Burner <alex.burner@gmail.com>
2017-07-30 10:58:19 -0700

    Style hours & meridian line

--- src/components/Disc/Time/Hours.tsx
+++ src/components/Disc/Time/Hours.tsx
@@ -56,19 +56,48 @@ const getHours = (zenithDate: Date, zenithTime: number): Moment[] => {
 
 export default ({ sunDict }: Props) => {
     const hours = getHours(sunDict.solarNoon.date, sunDict.solarNoon.time);
+    const fudge = -2; // over-clipping in Disc (to cover Colors strangeness)
+    const padding = 6;
+    const segment = 6;
     return (
-        <g>
+        <g style={{ opacity: 0.4 }}>
             {_.map(hours, ({angle, point, text}: Moment) => (
                 <g transform={`rotate(${angle} ${point.x} ${point.y})`}>
                     <text
-                        x={point.x}
+                        x={point.x + padding + segment + padding + fudge}
                         y={point.y}
                         dominantBaseline="middle"
+                        style={{
+                            fill: '#000',
+                            fontSize: '12px',
+                        }}
                     >
                         {text}
                     </text>
+                    <line
+                        x1={point.x + padding + fudge}
+                        y1={point.y}
+                        x2={point.x + padding + segment + fudge}
+                        y2={point.y}
+                        style={{
+                            stroke: '#000',
+                            strokeWidth: 1,
+                        }}
+                    />
                 </g>
             ))}
+            <line
+                x1={hours[0].point.x}
+                y1={hours[0].point.y}
+                x2={hours[12].point.x}
+                y2={hours[12].point.y}
+                strokeDasharray="5, 5"
+                style={{
+                    opacity: 0.6,
+                    stroke: '#000',
+                    strokeWidth: 1,
+                }}
+            />
         </g>
     );
 };
