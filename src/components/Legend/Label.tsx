60edc959cf724b69432212e82b63811b3ef476d3
Alex Burner <alex.burner@gmail.com>
2017-07-30 21:14:32 -0700

    Run build

---
+++ src/components/Legend/Label.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+interface Props {
+    color: string;
+    name: string;
+    start: Date;
+    stop: Date;
+}
+
+export default ({ color, name, start, stop }: Props) => (
+    <div></div>
+);
