24ff5302238a04ede8123c9525ca83526bc94820
Alex Burner <alex.burner@gmail.com>
2017-07-30 21:59:40 -0700

    Cache geolocation

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,13 @@
+{
+    // TODO TEMP allow flushing cached position with #reset hash
+    const hash = window.location.hash;
+    const reset = '#reset';
+    if (hash === reset) {
+        window.localStorage.removeItem('position');
+        window.location.href = window.location.href.slice(0, -reset.length);
+    }
+}
+
 import * as _ from 'lodash';
 import * as React from 'react';
 import * as suncalc from 'suncalc';
@@ -22,31 +32,44 @@ export default class App extends React.Component<{}, State> {
         };
     }
 
+    setSunState(latitude: number, longitude: number) {
+        const sunCalcs: SunCalcs = suncalc.getTimes(
+            new Date(),
+            latitude,
+            longitude
+        );
+        const zenithTime = sunCalcs.solarNoon.getTime();
+        const sunDict: SunDict = _.reduce(
+            sunCalcs,
+            (dict: SunDict, date: Date, key: string) => {
+                const time = date.getTime();
+                const angle = getTimeAngle(time, zenithTime);
+                const point = getCirclePoint(angle);
+                const moment: SunMoment = { date, time, angle, point };
+                dict[key] = moment;
+                return dict;
+            },
+            {}
+        );
+        this.setState({ latitude, longitude, sunDict });
+        window.localStorage.setItem('position', `${latitude},${longitude}`);
+    }
+
     componentDidMount() {
         if (this.state.sunDict) return;
-        navigator.geolocation.getCurrentPosition((position: Position) => {
-            const latitude = position.coords.latitude;
-            const longitude = position.coords.longitude;
-            const sunCalcs: SunCalcs = suncalc.getTimes(
-                new Date(),
-                latitude,
-                longitude
-            );
-            const zenithTime = sunCalcs.solarNoon.getTime();
-            const sunDict: SunDict = _.reduce(
-                sunCalcs,
-                (dict: SunDict, date: Date, key: string) => {
-                    const time = date.getTime();
-                    const angle = getTimeAngle(time, zenithTime);
-                    const point = getCirclePoint(angle);
-                    const moment: SunMoment = { date, time, angle, point };
-                    dict[key] = moment;
-                    return dict;
-                },
-                {}
-            );
-            this.setState({ latitude, longitude, sunDict });
-        }, (e: PositionError) => alert(e.message));
+        const position = window.localStorage.getItem('position');
+        if (position && position.length) {
+            const parts = position.split(',');
+            const latitude = Number(parts[0]);
+            const longitude = Number(parts[1]);
+            this.setSunState(latitude, longitude);
+        } else {
+            navigator.geolocation.getCurrentPosition((position: Position) => {
+                const latitude = position.coords.latitude;
+                const longitude = position.coords.longitude;
+                this.setSunState(latitude, longitude);
+            }, (e: PositionError) => alert(e.message));
+        }
     }
 
     render() {
